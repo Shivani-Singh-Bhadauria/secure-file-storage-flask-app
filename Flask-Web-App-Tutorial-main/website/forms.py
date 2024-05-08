85758f1a30b68b97422615c7fe80ec27394a2180
Shivani-Singh-Bhadauria <83387584+Shivani-Singh-Bhadauria@users.noreply.github.com>
2024-05-08 12:09:55 +0530

    Delete Flask-Web-App-Tutorial-main directory

--- Flask-Web-App-Tutorial-main/website/forms.py
+++
@@ -1,7 +0,0 @@
-# forms.py
-
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
-
-class UploadForm(FlaskForm):
-    file = FileField('Upload File', validators=[FileRequired()])
